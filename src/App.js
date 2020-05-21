3ba05673a1b88a02a51d5c02b126ac8e3ade0acf
dhk1349 <41980618+dhk1349@users.noreply.github.com>
2020-05-21 20:46:11 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './Components/Customer'
+
+const customers=[
+  {
+  'id':1,
+  'image':'https"//placeimage.com/64/64/1',
+  'name':'한동훈',
+  'birthday':'960403',
+  'gender':'남자',
+  'job':'대학생'
+  },
+  {
+    'id':2,
+    'image':'https"//placeimage.com/64/64/2',
+    'name':'한동훈',
+    'birthday':'960403',
+    'gender':'남자',
+    'job':'대학생'
+  },
+  {
+    'id':3,
+    'image':'https"//placeimage.com/64/64/3',
+    'name':'한동훈',
+    'birthday':'960403',
+    'gender':'남자',
+    'job':'대학생'
+  }
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management system!</h2>
+    <div>
+      {
+        customers.map(c=>{
+          return(
+            <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />
+          );
+        })
+      }
     </div>
   );
 }
